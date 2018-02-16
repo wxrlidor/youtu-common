516d7a53e4ba599a19be73796ba34a9bb1a8bf39
lidor <648799160@qq.com>
2018-02-17 01:08:40 +0800

    youtuResult类添加

---
+++ src/main/java/com/youtu/common/pojo/YouTuResult.java
@@ -0,0 +1,149 @@
+package com.youtu.common.pojo;
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+/**
+ * 通用的自定义响应结构
+ *@author:王贤锐
+ *@date:2018年1月1日  下午7:44:07
+**/
+public class YouTuResult {
+	 // 定义jackson对象
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    // 响应业务状态 200为正常
+    private Integer status;
+
+    // 响应消息
+    private String msg;
+
+    // 响应中的数据
+    private Object data;
+
+    public static YouTuResult build(Integer status, String msg, Object data) {
+        return new YouTuResult(status, msg, data);
+    }
+
+    public static YouTuResult ok(Object data) {
+        return new YouTuResult(data);
+    }
+
+    public static YouTuResult ok() {
+        return new YouTuResult(null);
+    }
+
+    public YouTuResult() {
+
+    }
+
+    public static YouTuResult build(Integer status, String msg) {
+        return new YouTuResult(status, msg, null);
+    }
+
+    public YouTuResult(Integer status, String msg, Object data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public YouTuResult(Object data) {
+        this.status = 200;
+        this.msg = "OK";
+        this.data = data;
+    }
+
+//    public Boolean isOK() {
+//        return this.status == 200;
+//    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    /**
+     * 将json结果集转化为YouTuResult对象
+     * 
+     * @param jsonData json数据
+     * @param clazz YouTuResult中的object类型
+     * @return
+     */
+    public static YouTuResult formatToPojo(String jsonData, Class<?> clazz) {
+        try {
+            if (clazz == null) {
+                return MAPPER.readValue(jsonData, YouTuResult.class);
+            }
+            JsonNode jsonNode = MAPPER.readTree(jsonData);
+            JsonNode data = jsonNode.get("data");
+            Object obj = null;
+            if (clazz != null) {
+                if (data.isObject()) {
+                    obj = MAPPER.readValue(data.traverse(), clazz);
+                } else if (data.isTextual()) {
+                    obj = MAPPER.readValue(data.asText(), clazz);
+                }
+            }
+            return build(jsonNode.get("status").intValue(), jsonNode.get("msg").asText(), obj);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * 没有object对象的转化
+     * 
+     * @param json
+     * @return
+     */
+    public static YouTuResult format(String json) {
+        try {
+            return MAPPER.readValue(json, YouTuResult.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Object是集合转化
+     * 
+     * @param jsonData json数据
+     * @param clazz 集合中的类型
+     * @return
+     */
+    public static YouTuResult formatToList(String jsonData, Class<?> clazz) {
+        try {
+            JsonNode jsonNode = MAPPER.readTree(jsonData);
+            JsonNode data = jsonNode.get("data");
+            Object obj = null;
+            if (data.isArray() && data.size() > 0) {
+                obj = MAPPER.readValue(data.traverse(),
+                        MAPPER.getTypeFactory().constructCollectionType(List.class, clazz));
+            }
+            return build(jsonNode.get("status").intValue(), jsonNode.get("msg").asText(), obj);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
